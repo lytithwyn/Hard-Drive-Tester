e9284a7b241ca82d35883ea6b653648674312e8f
Matthew Morgan <lytithwyn@gmail.com>
2011-04-08 14:35:58 -0400

    First Commit!

---
+++ Tester.rb
@@ -0,0 +1,79 @@
+require "gtk2"
+
+class Tester
+    def initialize
+        # scan the system for hard drives
+        findHardDrives
+
+        # now lets build our GUI
+        winMain = Gtk::Window.new
+        winMain.set_title("Hard Drive Tester")
+        winMain.border_width = 10
+
+        driveTable = Gtk::Table.new(@hardDrives.length, 3)
+        driveTable.column_spacings = 5
+        driveTable.row_spacings = 5
+
+        row = 0;
+        @hardDrives.each do |hardDrive|
+            hdLabel = Gtk::Label.new(hardDrive)
+
+            qsButton = Gtk::Button.new("Quick Scan")
+            fsButton = Gtk::Button.new("Full Scan")
+
+            qsButton.signal_connect("clicked") do
+                quickHardDriveTest(hardDrive)
+            end
+
+            fsButton.signal_connect("clicked") do
+                fullHardDriveTest(hardDrive)
+            end
+
+            driveTable.attach_defaults(hdLabel, 0, 1, row, row + 1)
+            driveTable.attach_defaults(qsButton, 1, 2, row, row + 1)
+            driveTable.attach_defaults(fsButton, 2, 3, row, row + 1)
+            row += 1
+        end
+
+        winMain.signal_connect('delete_event') do
+            false
+        end
+
+        winMain.signal_connect('destroy') do
+            Gtk.main_quit
+        end
+
+        winMain.add(driveTable)
+        winMain.show_all
+        Gtk.main
+    end
+
+    def findHardDrives
+        @hardDrives = Array.new
+        begin
+            # lines look like this
+            #   8        0 156290904 sda
+            #   8        1  81923436 sda1
+            #   8        2    104422 sda2
+            hdRegex = Regexp.new(/\s+\d\s+\d\s+\d+\s+([a-zA-Z]+)(?!\d)$/);
+            File.open("/proc/partitions", "r") do |partitions|
+                while(line = partitions.gets)
+                    hdRegex.match(line) do |hdMatch|
+                        @hardDrives << "/dev/" + hdMatch[1]
+                    end
+                end
+            end
+        rescue
+            puts "We failed! " + $!
+        end
+    end
+
+    def fullHardDriveTest(hardDrive)
+        system "xterm -fg white -bg black -e vdt --read --wait -r " + hardDrive + "  &"
+    end
+
+    def quickHardDriveTest(hardDrive)
+        puts "Just pretending to run a quick test on " + hardDrive
+    end
+end
+
---
+++ hardDriveTest.rb
@@ -0,0 +1,3 @@
+require "Tester.rb"
+
+myTester = Tester.new
